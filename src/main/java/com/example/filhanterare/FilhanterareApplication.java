457315f7b6ca2ec37c494a7cd5f3592b6c605b01
kevin nguyen <kevin01nguyen@hotmail.com>
2022-12-19 10:34:03 +0100

    Initial commit

---
+++ src/main/java/com/example/filhanterare/FilhanterareApplication.java
@@ -0,0 +1,13 @@
+package com.example.filhanterare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilhanterareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilhanterareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/filhanterare/FilhanterareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filhanterare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilhanterareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
