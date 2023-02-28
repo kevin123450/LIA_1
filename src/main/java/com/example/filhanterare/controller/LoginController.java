84990af7bd223753d69dad943f8af54d8f95c472
kevin nguyen <kevin01nguyen@hotmail.com>
2023-02-28 11:16:17 +0100

    delete file commit

--- src/main/java/com/example/filhanterare/controller/LoginController.java
+++ src/main/java/com/example/filhanterare/controller/LoginController.java
@@ -1,19 +1,14 @@
 package com.example.filhanterare.controller;
 
-import com.example.filhanterare.dto.CustomerDTO;
 import com.example.filhanterare.dto.LoginRequestDTO;
-import com.example.filhanterare.dto.SignUpRequestDTO;
 import com.example.filhanterare.dto.WhoAmIDTO;
 import com.example.filhanterare.service.LoginService;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = {"http://localhost:3000"}, methods = {
-        RequestMethod.GET, RequestMethod.DELETE, RequestMethod.PUT, RequestMethod.POST, RequestMethod.OPTIONS
-})
+@CrossOrigin
 public class LoginController {
 
     private final LoginService loginService;
@@ -30,28 +25,5 @@ public class LoginController {
     @GetMapping("/whoami")
     public WhoAmIDTO whoAmI(@RequestParam String token) {
         return loginService.whoAmI(token);
-    }
-
-    @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequestDTO signUpRequestDTO) {
-        return loginService.signUp(
-                signUpRequestDTO.username(),
-                signUpRequestDTO.password(),
-                signUpRequestDTO.email(),
-                signUpRequestDTO.roles()
-        );
-    }
-
-    @PostMapping("/customer-signup")
-    public ResponseEntity<?> registerCustomer(@Valid @RequestBody CustomerDTO customerDTO) {
-        return loginService.customerSignUp(
-                customerDTO.id(),
-                customerDTO.firstname(),
-                customerDTO.lastname(),
-                customerDTO.address(),
-                customerDTO.zipcode(),
-                customerDTO.city(),
-                customerDTO.customerType()
-        );
     }
 }
